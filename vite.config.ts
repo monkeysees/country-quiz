95aca702352eb0127170c7c90caea6157f7fc37a
islambeg katibov <islambeg@protonmail.com>
2022-06-14 15:31:52 +0000

    Add static assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,41 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    svgLoader({
+      defaultImport: "url",
+      svgoConfig: {
+        multipass: true,
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                removeViewBox: false,
+                removeTitle: false,
+                cleanupIDs: false,
+                removeUnknownsAndDefaults: {
+                  keepAriaAttrs: true,
+                  keepRoleAttr: true,
+                },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  publicDir: "assets",
+  publicDir: "static",
   base: "/frameworks-land/country-quiz/",
   build: {
     outDir: "public",
